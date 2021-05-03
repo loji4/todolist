0e47d82597100c39cec968dc4a5590bd472e5255
loji4 <yogawithlindao@gmail.com>
2021-05-03 14:19:27 +1000

    added HTML and pseudocode

---
+++ app.js
@@ -0,0 +1,12 @@
+// create an empty array 
+// initialize and name empty array 
+// select the button  and add an event listener 
+// in the function, select the input box and the value of the input box
+// push the value into the array 
+// then call the update function 
+// update function will take the value and add a paragraph button and text Node, 
+// add it to the entire to do list 
+
+let toDoListItems = []
+i = 0 
+
